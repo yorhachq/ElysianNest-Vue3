c233749ec1ffdb49339c8bd82e037c8051b760e3
yorhachq <yorhachq@gmail.com>
2024-05-02 18:02:50 +0800

    fix: Changed build target from es2015 to es2022

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     },
     build: {
       // https://cn.vitejs.dev/guide/build.html#browser-compatibility
-      target: "es2015",
+      target: "es2022",
       sourcemap: false,
       // 消除打包大小超过500kb警告
       chunkSizeWarningLimit: 4000,
